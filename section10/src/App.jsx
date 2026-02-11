e3771541821a40bc7184ad1ecf2ef0443d1a06b9
dldydtjr003 <dldydtjr002@gmail.com>
2026-02-11 14:10:36 +0900

    [생성] useMemo hooks 테스팅

--- section10/src/App.jsx
+++ section10/src/App.jsx
@@ -1,48 +1,94 @@
-import { useState } from 'react'
-import Viewer from './components/Viewer'
-import InputController from './components/InputController'
-
 import './App.css'
+import './css/App.css'
+import Header from './components/Header'
+import Editor from './components/Editor'
+import List from './components/List'
+import { useState, useRef, useReducer } from 'react'
+import Exam from './components/Exam'
+
+// 전역변수
+const mockData = [ 
+  { 
+    id: 0, 
+    isDone: false, 
+    content: "React 공부하기", 
+    date: new Date().getTime(), 
+  }, 
+  { 
+    id: 1, 
+    isDone: false, 
+    content: "Spring Boot 공부하기", 
+    date: new Date().getTime(), 
+  }, 
+  { 
+    id: 2, 
+    isDone: false, 
+    content: "Java 공부하기", 
+    date: new Date().getTime(), 
+  }, 
+]; 
+function reducer(data, action) {
+  switch (action.type) {
+    case "CREATE":
+      return [action.data, ...data]
+  
+    case "DELETE":
+      return data.filter((item)=>{
+        return item.id !== action.id
+        }
+      )
+  
+    case "UPDATE":
+      return data.map((item)=>{
+        return item.id === action.id ?{...item, isDone: !item.isDone}:item
+      })
+  
+    default:
+      return data
+  }
+}
 
 function App() {
-  const [count, setCount] = useState(0);
+  //const[todos, setTodos] = useState(mockData);
+  const[todos,dispatch] = useReducer(reducer,mockData);
+  const idRef = useRef(3);
 
-
-    const onClick = (num1,num2,operator)=>{
-      let sum = 0;
-      switch (operator) {
-        case '+':
-          sum = parseInt(num1) + parseInt(num2);
-          break;
-        case '-':
-          sum = parseInt(num1) - parseInt(num2);
-          break;
-        case '*':
-          sum = parseInt(num1) * parseInt(num2);
-          break;
-        case '/':
-          sum = parseInt(num1) / parseInt(num2);
-          break;
-      }
-      setCount(sum)
-      
+  // 이벤트 함수 생성(setTodos 핸들러 함수)
+  const onCreate = (content)=>{
+    dispatch({
+    type:"CREATE",
+    data:{
+      id: idRef.current++, 
+      isDone: false, 
+      content: content, 
+      date: new Date().getTime(), 
     }
-    
+    })
+  }
+  // 이벤트 함수 생성(setTodos 수정)
+  const onUpdate = (id)=>{
+    dispatch({
+    type:"UPDATE",
+    id:id
+    })
+  }
+  // 이벤트 함수 생성(setTodos 삭제)
+  const onDelete = (id)=>{
+    dispatch({
+      type:"DELETE",
+      id:id
+    })
+  }
   return (
     <>
-      <div className="App">
-        <div>
-          <h1>계산기</h1>
-        </div>
-        
-        <div className="Viewer">
-          <Viewer count = {count}  />
-        </div>
-        
-        <div className="InputController">
-          <InputController onClick = {onClick}/>
-        </div>
-      </div>
+    <div className="App">
+      <Header />
+      <Exam />
+      <Editor onCreate = {onCreate}/>
+      <List todos = {todos} onUpdate = {onUpdate} onDelete ={onDelete}/>
+
+    </div>
+
     </>
   )
 }
---
+++ section10/src/components/List.jsx
@@ -0,0 +1,51 @@
+import { useState,useMemo } from 'react';
+import '../css/List.css'
+import TodoItem from './TodoItem';
+
+
+const List = ({todos, onUpdate, onDelete})=>{
+  const [search,setSearch] = useState('');
+
+  const onChangeSearch=(e)=>{
+    setSearch(e.target.value);
+  }
+  // 전체 리스트 갯수, 완료 갯수, 미완료 갯수
+  const { totalCount, doneCount, notDoneCount } = useMemo(()=>{
+    console.log("전체갯수");
+    let totalCount = todos.length;
+    let doneCount = todos.filter((todo)=>todo.isDone === true).length
+    let notDoneCount = totalCount - doneCount;
+    return {totalCount,doneCount,notDoneCount};
+  },[todos])
+
+  // 주의!! 
+  const getFilterData = ()=>{
+    if(search === ''){
+      return todos;
+    }
+    return todos.filter((todo)=>{
+      return todo.content.toLowerCase().includes(search.toLowerCase())
+    })
+  }
+  const filterTodos = getFilterData();
+  
+  
+  
+  return <>
+    <div className="List"> 
+      <div>
+        <p>total:{totalCount}</p>
+        <p>done:{doneCount}</p>
+        <p>notDone:{notDoneCount}</p>
+      </div>
+      <h4>Todo List </h4> 
+      <input value={search} onChange={onChangeSearch} placeholder="검색어를 입력하세요" /> 
+      <div className="todos_wrapper"> 
+        {filterTodos.map((todo)=>{
+          return <TodoItem key={todo.id} {...todo} onUpdate = {onUpdate} onDelete = {onDelete}/>
+        })} 
+      </div> 
+    </div> 
+  </>
+}
+export default List;
