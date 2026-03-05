b31dfd5594a933632975f57662f0213fd667bad5
dldydtjr003 <dldydtjr002@gmail.com>
2026-03-05 14:46:44 +0900

    [추가] 동적페이지 이동( useNavigate) 기능 테스팅

--- mall/src/pages/todo/ModifyPage.jsx
+++ mall/src/pages/todo/ModifyPage.jsx
@@ -1,14 +1,22 @@
 import Header from "../../include/header";
-import { useParams } from "react-router-dom";
+import { useParams, useSearchParams } from "react-router-dom";
 import "./ReadPage.css";
 
 const ModifyPage = () => {
   const { tno } = useParams();
+  const [queryParams] = useSearchParams();
+
+  const page = queryParams.get("page") ? parseInt(queryParams.get("page")) : 1;
+  const size = queryParams.get("size") ? parseInt(queryParams.get("size")) : 10;
+
   return (
     <>
       <div className="main-container">
         <Header />
         <p>Modify Page tno = {tno}</p>
+        <p>
+          Modify page = {page} size = {size}
+        </p>
         <main className="content-area">
           <div className="button-wrapper">
             <button type="button" className="custom-btn-outline">
--- mall/src/pages/todo/ReadPage.jsx
+++ mall/src/pages/todo/ReadPage.jsx
@@ -1,23 +1,36 @@
-import { useCallback } from "react";
 import Header from "../../include/header";
 import "./ReadPage.css";
-import { useParams, useNavigate } from "react-router-dom";
+import { useCallback } from "react";
+import {
+  useParams,
+  useNavigate,
+  useSearchParams,
+  createSearchParams,
+} from "react-router-dom";
 
 const ReadPage = () => {
   const { tno } = useParams();
   const navigate = useNavigate();
+  const [queryParams] = useSearchParams();
+
+  const page = queryParams.get("page") ? parseInt(queryParams.get("page")) : 1;
+  const size = queryParams.get("size") ? parseInt(queryParams.get("size")) : 10;
+  // "?page=1&size=10"
+  const queryStr = createSearchParams({ page, size }).toString();
 
   // 동적페이지 이동
   const moveModify = useCallback(() => {
     navigate({
       pathname: `/todo/modify/${tno}`,
+      search: queryStr,
     });
-  }, [navigate, tno]);
+  }, [navigate, tno, queryStr]);
   return (
     <>
       <div className="main-container">
         <Header />
         <p>Read Page tno = {tno}</p>
+
         <main className="content-area">
           <div className="button-wrapper">
             <button type="button" className="custom-btn-outline">
